324676e7844b41a320fa572e71f598e560f53cb8
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-09-06 14:47:09 +0500

    Added return to manager page

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.http import HttpResponseRedirect
+from django.utils.http import url_has_allowed_host_and_scheme
+
 
 from .models import Product
 from .models import ProductCategory
@@ -123,6 +126,18 @@ class OrderAdmin(admin.ModelAdmin):
         for obj in formset.deleted_objects:
             obj.delete()
         formset.save_m2m()
+
+    def response_change(self, request, obj):
+        next_url = request.GET.get('next')
+
+        if next_url and url_has_allowed_host_and_scheme(
+            url=next_url,
+            allowed_hosts={request.get_host()},
+            require_https=request.is_secure(),
+        ):
+            return HttpResponseRedirect(next_url)
+
+        return super().response_change(request, obj)
 
 
 @admin.register(ProductCategory)
