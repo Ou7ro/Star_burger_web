c04b47d0e7e31512d2d03554e065a62629884f18
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-09-07 14:58:43 +0500

    Fix some issues

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -144,8 +144,9 @@ class Order(models.Model):
         ('completed', 'Завершен'),
     ]
     PAYMANT_CHOICES = [
+        ('unprocessed', 'Необработанный'),
         ('cash', 'Наличностью'),
-        ('electronic', 'Электронно')
+        ('electronic', 'Электронно'),
     ]
     status = models.CharField(
         max_length=20,
@@ -157,7 +158,7 @@ class Order(models.Model):
     payment_method = models.CharField(
         max_length=20,
         choices=PAYMANT_CHOICES,
-        default='Не выбран',
+        default='unprocessed',
         db_index=True,
         verbose_name='Способ оплаты'
     )
