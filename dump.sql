caf5e9144ff4dbac76d5988d7be9c0274ca45ff1
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-12-27 12:50:05 +0500

    Докеризация на локалке

--- dump.sql
+++ dump.sql
@@ -23,16 +23,10 @@ SET row_security = off;
 -- Name: public; Type: SCHEMA; Schema: -; Owner: pg_database_owner
 --
 
-CREATE SCHEMA public;
-
-
-ALTER SCHEMA public OWNER TO pg_database_owner;
-
 --
 -- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: pg_database_owner
 --
 
-COMMENT ON SCHEMA public IS 'standard public schema';
 
 
 SET default_tablespace = '';
